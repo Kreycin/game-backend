a853786aabe6cd88d2f2f550c7027bd350ef1da1
Nick <nick@MacBookAirNick.local>
2025-06-20 16:54:58 +0700

    feat: add public url to server config

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,8 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  // --- ★★★ เพิ่มบรรทัดนี้เข้าไปตามแผน B ของเรา ★★★ ---
+  url: 'https://game-backend-demonslayer.onrender.com', 
   app: {
     keys: env.array('APP_KEYS'),
   },
