93f5601b79557ba22ed007d4f517e147e647e871
Nick <nick@MacBookAirNick.local>
2025-06-20 17:07:32 +0700

    fix: set absolute public url for server

--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,11 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  // --- ★★★ เพิ่มบรรทัดนี้เข้าไปตามแผน B ของเรา ★★★ ---
+
+  // บรรทัดนี้สำคัญที่สุด! 
+  // ต้องมี 'url' และค่าของมันต้องเป็น URL ของ Render backend
   url: 'https://game-backend-demonslayer.onrender.com', 
+
   app: {
     keys: env.array('APP_KEYS'),
   },
