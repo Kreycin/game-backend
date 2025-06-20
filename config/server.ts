774abe172893d05e244ba93209236f28188aaf34
Nick <nick@MacBookAirNick.local>
2025-06-20 18:31:48 +0700

    refactor: remove public url from server config for cloudinary

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,6 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-
-  // บรรทัดนี้สำคัญที่สุด! 
-  // ต้องมี 'url' และค่าของมันต้องเป็น URL ของ Render backend
-  url: 'https://game-backend-demonslayer.onrender.com', 
-
   app: {
     keys: env.array('APP_KEYS'),
   },
