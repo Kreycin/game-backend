3dc1a2a50b1973a0eaf427392d3075431f078fde
Nick <nick@MacBookAirNick.local>
2025-07-02 21:15:18 +0700

    fix: explicitly define all site-counter routes

--- src/api/site-counter/routes/site-counter.ts
+++ src/api/site-counter/routes/site-counter.ts
@@ -1,5 +1,14 @@
+// src/api/site-counter/routes/site-counter.ts
 export default {
   routes: [
+    {
+      method: 'GET',
+      path: '/site-counter',
+      handler: 'site-counter.find',
+      config: {
+        auth: false,
+      },
+    },
     {
       method: 'PUT',
       path: '/site-counter/increment',
