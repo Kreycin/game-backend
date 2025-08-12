ebd99cab3116e573d09e70ce14566c92f1a0d229
Nick <nick@MacBookAirNick.local>
2025-08-12 18:10:15 +0700

    fix: Make tier-list-guide API public

--- dist/src/api/tier-list-guide/routes/tier-list-guide.js
+++ dist/src/api/tier-list-guide/routes/tier-list-guide.js
@@ -1,7 +1,15 @@
-"use strict";
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * tier-list-guide router
  */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreRouter('api::tier-list-guide.tier-list-guide');
+const { createCoreRouter } = require('@strapi/strapi').factories;
+// เราจะแก้ไขส่วนนี้เพื่อทำให้ API เป็น Public
+exports.default = createCoreRouter('api::tier-list-guide.tier-list-guide', {
+    config: {
+        find: {
+            // ทำให้ find (การดึงข้อมูล) ไม่ต้องใช้ token หรือการยืนยันตัวตน
+            auth: false,
+        },
+    },
+});
--- src/api/tier-list-guide/routes/tier-list-guide.ts
+++ src/api/tier-list-guide/routes/tier-list-guide.ts
@@ -1,7 +1,17 @@
+'use strict';
+
 /**
  * tier-list-guide router
  */
 
-import { factories } from '@strapi/strapi';
+const { createCoreRouter } = require('@strapi/strapi').factories;
 
-export default factories.createCoreRouter('api::tier-list-guide.tier-list-guide');
+// เราจะแก้ไขส่วนนี้เพื่อทำให้ API เป็น Public
+export default createCoreRouter('api::tier-list-guide.tier-list-guide', {
+  config: {
+    find: {
+      // ทำให้ find (การดึงข้อมูล) ไม่ต้องใช้ token หรือการยืนยันตัวตน
+      auth: false,
+    },
+  },
+});
