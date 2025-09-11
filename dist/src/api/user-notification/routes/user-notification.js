b7647f4d8bf10deca52d3f8a0137bd674911015c
Nick <nick@MacBookAirNick.local>
2025-09-11 11:09:57 +0700

    d

--- dist/src/api/user-notification/routes/user-notification.js
+++ dist/src/api/user-notification/routes/user-notification.js
@@ -1,23 +1,8 @@
-// api/user-notification/routes/user-notification.ts
+// api/user-notification/controllers/user-notification.ts
 'use strict';
 Object.defineProperty(exports, "__esModule", { value: true });
 /**
- * user-notification router.
+ * user-notification controller
  */
 const strapi_1 = require("@strapi/strapi");
-// เราจะเพิ่ม config เข้าไปใน createCoreRouter
-exports.default = strapi_1.factories.createCoreRouter('api::user-notification.user-notification', {
-    config: {
-        // --- VVV เพิ่ม config เข้าไปใน actions ที่ต้องการทดสอบ VVV ---
-        find: {
-            auth: false, // ปิดการยืนยันตัวตนสำหรับ "โหลดข้อมูล"
-        },
-        create: {
-            auth: false, // ปิดการยืนยันตัวตนสำหรับ "สร้างข้อมูล"
-        },
-        update: {
-            auth: false, // ปิดการยืนยันตัวตนสำหรับ "อัปเดตข้อมูล"
-        },
-        // --------------------------------------------------------
-    }
-});
+exports.default = strapi_1.factories.createCoreController('api::user-notification.user-notification');
--- src/api/user-notification/routes/user-notification.ts
+++ src/api/user-notification/routes/user-notification.ts
@@ -1,25 +1,10 @@
-// api/user-notification/routes/user-notification.ts
+// api/user-notification/controllers/user-notification.ts
 'use strict';
 
 /**
- * user-notification router.
+ * user-notification controller
  */
 
 import { factories } from '@strapi/strapi';
 
-// เราจะเพิ่ม config เข้าไปใน createCoreRouter
-export default factories.createCoreRouter('api::user-notification.user-notification', {
-  config: {
-    // --- VVV เพิ่ม config เข้าไปใน actions ที่ต้องการทดสอบ VVV ---
-    find: {
-      auth: false, // ปิดการยืนยันตัวตนสำหรับ "โหลดข้อมูล"
-    },
-    create: {
-      auth: false, // ปิดการยืนยันตัวตนสำหรับ "สร้างข้อมูล"
-    },
-    update: {
-      auth: false, // ปิดการยืนยันตัวตนสำหรับ "อัปเดตข้อมูล"
-    },
-    // --------------------------------------------------------
-  }
-});
+export default factories.createCoreController('api::user-notification.user-notification');
