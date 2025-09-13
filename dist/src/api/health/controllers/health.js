201171ac78fcce6c6767a4cf3c3ba962b93d3a54
Nick <nick@MacBookAirNick.local>
2025-09-13 14:26:58 +0700

    strpi

--- dist/src/api/health/controllers/health.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// src/api/health/controllers/health.ts
-exports.default = {
-    async check(ctx) {
-        ctx.body = { status: 'ok' };
-    },
-};
